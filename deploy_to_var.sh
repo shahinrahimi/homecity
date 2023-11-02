1233ccde4b6d58f06750d0633d27a67ed55b3d0d
Shahin Rahimi <sh.rahimi.3d@gmail.com>
2023-11-02 19:04:50 +0300

    simple fix

--- deploy_to_var.sh
+++ deploy_to_var.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ADMIN_SRC_DIR="./admin/dist"
-ADMIN_DEST_DIR="/var/www/homecity/admin/"
+ADMIN_DEST_DIR="/var/www/homecity/admin-build/"
 
 cp -r $ADMIN_SRC_DIR/* $ADMIN_DEST_DIR
 
 CLINET_SRC_DIR="./client/dist"
-CLIENT_DEST_DIR="/var/www/homecity/client/"
+CLIENT_DEST_DIR="/var/www/homecity/client-build/"
 
 cp -r $CLINET_SRC_DIR/* $CLIENT_DEST_DIR
 
