543d8cec02f08e21beb413e19003b11627fa34c9
Shahin Rahimi <sh.rahimi.3d@gmail.com>
2023-11-02 18:59:36 +0300

    add bash file

---
+++ deploy_to_var.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ADMIN_SRC_DIR="./admin/dist/"
+ADMIN_DEST_DIR="/var/www/homecity/admin/"
+
+cp -r $ADMIN_SRC_DIR/* $ADMIN_DEST_DIR
+
+CLINET_SRC_DIR="./client/dist/"
+CLIENT_DEST_DIR="/var/www/homecity/client/"
+
+cp -r $CLINET_SRC_DIR/* $CLIENT_DEST_DIR
+
+
