92f77b6a463f3c76b6a888f2b6c9013d646b981a
shahin <sh.rahimi.3d@gmail.com>
2023-11-18 14:48:44 +0300

    adding domain to allowed origins, updating gitigone file

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:5173', // vite
     `http://localhost:${process.env.PORT}`,
+    'http://homecitygroup.com.tr',
+    'http://www.homecitygroup.com.tr'
 ]
 
 module.exports = allowedOrigins
