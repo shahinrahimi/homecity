5871ea3b655693dfee6aad7c7c852787184f89d9
shahin <sh.rahimi.3d@gmail.com>
2023-11-18 14:57:50 +0300

    adding https version to cors

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -2,7 +2,9 @@ const allowedOrigins = [
     'http://localhost:5173', // vite
     `http://localhost:${process.env.PORT}`,
     'http://homecitygroup.com.tr',
-    'http://www.homecitygroup.com.tr'
+    'http://www.homecitygroup.com.tr',
+    'https://homecitygroup.com.tr',
+    'https://www.homecitygroup.com.tr',
 ]
 
 module.exports = allowedOrigins
